8ad5669c876a4d0a28ad844090384fa9a17c46a4
prashantgit2804 <prashantgithub49@gmail.com>
2026-03-27 09:18:29 +0530

    changes done

--- src/main/java/com/student1/demo/controller/HelloController.java
+++ src/main/java/com/student1/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
 
-        return "Hello from Student Java App ";
+        return "Hello from Student Java App   ";
 
     }
 }
